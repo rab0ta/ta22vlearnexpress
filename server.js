81894d5b29b0722f4c6cf08b92793105ca894a33
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-04-10 09:20:08 +0300

    Add nunjucks

--- server.js
+++ server.js
@@ -2,20 +2,26 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
 app.use(bodyParser.urlencoded({extended:true}));
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
 
 // req => request , res => response
 app.get('/', (req, res) => {
     console.log(req.query);
-    res.sendFile(__dirname + '/views/index.html');
+    res.render('index.njk');
 });
 app.get('/answer', (req, res) => {
     console.log(req.query);
-    res.send(req.query);
+    res.render('answer.njk', req.query);
 });
 app.post('/answer', (req, res) => {
     console.log(req.body);
-    res.send(req.body);
+    res.render('answer.njk', {...req.query, ...req.body});
 });
 
 app.listen(port, () => {
